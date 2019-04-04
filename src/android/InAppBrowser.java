24a64314c1a329f0b05694a50cdbc51b8de37955
worksection <info@worksection.com>
2019-04-04 16:39:43 +0300

    Update InAppBrowser.java

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -815,19 +815,6 @@ public class InAppBrowser extends CordovaPlugin {
                 dialog.getWindow().getAttributes().windowAnimations = android.R.style.Animation_Dialog;
                 dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
                    
-        try {
-            Method getSettings = dialog.getWindow().getClass().getMethod("getSettings");
-            Object wSettings = getSettings.invoke(dialog.getWindow());
-            Method setTextZoom = wSettings.getClass().getMethod("setTextZoom", Integer.TYPE);
-            setTextZoom.invoke(wSettings, 100);
-        } catch (ClassCastException ce) {
-        } catch (NoSuchMethodException e) {
-        } catch (InvocationTargetException e) {
-        } catch (IllegalAccessException e) {
-        }
-                   
-                   
-                   
                 dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
                 dialog.setCancelable(true);
                 dialog.setInAppBroswer(getInAppBrowser());
@@ -1008,6 +995,19 @@ public class InAppBrowser extends CordovaPlugin {
                     }
 
                 });
+                
+                try {
+                    Method getSettings = dialog.getWindow().getClass().getMethod("getSettings");
+                    Object wSettings = getSettings.invoke(dialog.getWindow());
+                    Method setTextZoom = wSettings.getClass().getMethod("setTextZoom", Integer.TYPE);
+                    setTextZoom.invoke(wSettings, 100);
+                } catch (ClassCastException ce) {
+                } catch (NoSuchMethodException e) {
+                } catch (InvocationTargetException e) {
+                } catch (IllegalAccessException e) {
+                }
+
+
                 WebViewClient client = new InAppBrowserClient(thatWebView, edittext, beforeload);
                 inAppWebView.setWebViewClient(client);
                 WebSettings settings = inAppWebView.getSettings();
